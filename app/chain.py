f4f3ba2f3fc16bde83799ec70b691eb630b2bb3c
Amit raj <amit.rock9889@gmail.com>
2026-01-07 18:45:19 +0530

    CHanges llm model

--- app/chain.py
+++ app/chain.py
@@ -1,12 +1,14 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 
 llm = ChatGroq(
     groq_api_key=os.getenv("GROQ_API_KEY"),
-    model_name="llama-3.3-70b-versatile"
+    model_name="llama-3.1-8b-instant"
 )
+
 
 
 prompt = PromptTemplate(
